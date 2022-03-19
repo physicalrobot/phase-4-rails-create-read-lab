9d700d1560e8cb471ed8429506161b2a0d0e6702
Vikalp Malhotra <kimboho@gmail.com>
2022-03-19 15:23:48 -0400

    commit tree 37212a86b5725515e7dad1fd0360aea1c001582d

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,23 @@
+class PlantsController < ApplicationController
+
+    def index 
+        plants = Plant.all
+        render json: plants 
+    end
+
+    def show 
+        plants = Plant.find_by(id: params[:id])
+        render json: plants
+    end
+
+    def create 
+        plants = Plant.create(plant_params)
+        render json: plants, status: :created
+    end
+
+    private
+
+    def plant_params 
+        params.permit(:id, :name, :image, :price)
+    end
+end
